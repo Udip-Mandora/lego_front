9c904958fa25ba036f736279b676cf063f13b4cd
Udip <udipmandora42@gmail.com>
2023-06-20 15:22:50 -0400

    added search functionality

--- src/myComponent/header.js
+++ src/myComponent/header.js
@@ -1,21 +1,65 @@
 // header.js
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import About from '../About';
+import MyComponent from '../App';
+import { Form } from 'react-bootstrap';
+import { InputGroup } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const Header = () => {
 
-    const [search, setSearch] = useState('');
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost/api/colors');
+                setData(response.data);
+            }
+            catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    // console.log(data);
+    const [value, setValue] = useState('');
+    // console.log(value);
+
     const onChange = (e) => {
-        setSearch(e.target.value);
+        setValue(e.target.value);
     }
+
+    const onSearch = (searchTerm) => {
+        setValue(searchTerm);
+        console.log('search', searchTerm);
+    }
+
+
     return (
         <header className="header-navigation">
             <h2 className='hero-title'>LEGO Colors</h2>
             <div className="navigation-menu">
                 <ul className="navigation-items">
                     <li className='search-input'>
-                        <span className='fa fa-search'></span><input type='text' placeholder='Search Color' value={search} onChange={onChange} ></input>
+                        <form>
+                            <input type='text' value={value} onChange={onChange} placeholder='Search Color' />
+                            <button onClick={() => onSearch(value)}><span className='fa fa-search'></span></button>
+                        </form>
+                        <div className='dropdown'>
+                            {data.filter((item) => {
+                                const searchTerm = value.toLocaleLowerCase();
+                                const name = item.name.toLocaleLowerCase();
+
+                                return searchTerm && name.startsWith(searchTerm) && name !== searchTerm;
+                            }).slice(0, 10).map((item) => <div key={item.id} onClick={() => onSearch(item.name)} className='dropdown-row'>
+                                {item.name}</div>)}
+                        </div>
                     </li>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
